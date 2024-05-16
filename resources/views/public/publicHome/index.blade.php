c1f883d4412fb21de3da514da2b02253eb7185f8
Trung Kiên <131913502+anhkienit3011@users.noreply.github.com>
2024-05-17 02:36:44 +0700

    css
    upload css

--- resources/views/public/publicHome/index.blade.php
+++
@@ -1,2 +0,0 @@
-<x-public.layout>
-</x-public.layout>
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,10 @@ use App\Http\Controllers\public\ProductController;
 use App\Http\Controllers\public\PublicHomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
 
 // BEGIN::PUBLIC
-Route::get('/', [PublicHomeController::class, 'Index'])->name("Home.index");
+Route::get('/',[AppController::class,'index'])->name('public.index');
 Route::get('/home', [PublicHomeController::class, 'Index'])->name("Home.index");
 
 Route::get('/cart', [CartController::class, 'Index'])->name("cart.index");
