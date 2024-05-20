43a7ea39f27cee5132a42fc4ea617a3deefad456
Dai Dong <phungdaidong1114@gmail.com>
2024-05-20 15:42:16 +0700

    the end

--- app/Http/Controllers/public/CartController.php
+++ app/Http/Controllers/public/CartController.php
@@ -32,10 +32,6 @@ class CartController extends Controller
 
 
 
-    $invoiceDetails = InvoiceDetail::where('invoice_id', $latestInvoice->id)->get();
-    $total = $invoiceDetails->sum(function ($invoiceDetail) {
-        return $invoiceDetail->varient->product->price * $invoiceDetail->quantity;
-    });
         $invoiceDetails = InvoiceDetail::where('invoice_id', $latestInvoice->id)->get();
         $total = $invoiceDetails->sum(function ($invoiceDetail) {
             return $invoiceDetail->varient->product->price;
